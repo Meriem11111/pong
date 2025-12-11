7df51fc88778bfec4fc7b341163641b8457bc316
Meriem11111 <abdlk.meriem00@gmail.com>
2025-12-11 20:30:41 +0100

    created a GameRoom

--- backend/server.ts
+++ backend/server.ts
@@ -2,6 +2,10 @@ import Fastify from "fastify";
 import fastifyCors from "@fastify/cors";
 import { Server as SocketIOServer } from "socket.io";  
 
+export let playerID : string[] = [];
+export const gameRooms = new Map<string, { player1: string, player2: string }>();
+
+
 const server = Fastify({
     logger: true
 });
@@ -24,19 +28,61 @@ const gameSocket = new SocketIOServer(server.server, {
     }
 });
 
+function generateRoomID() : string {
+    const randomID = Math.floor(Math.random() * 1000);
+    return `room${randomID}`;
+}
+
 // Listen for connection
 gameSocket.on("connection", (socket) => {  
     console.log("✅ Client connected:", socket.id);
+    playerID.push(socket.id);
+    console.log("All player IDs connected : ", playerID);
+    
+    if(playerID.length >= 2)
+    {
+        
+        if (playerID[0] && playerID[1]) {
+            let newRoomID = generateRoomID();
+            while (gameRooms.has(newRoomID)) {
+               newRoomID = generateRoomID();
+            }
+            console.log("new ROOM_ID ::::  ",newRoomID);
+            gameRooms.set(newRoomID, {
+            player1: playerID[0],
+            player2: playerID[1]
+        });
+        playerID.splice(0,2);
+        const room = gameRooms.get(newRoomID);
+        console.log(room?.player1);
+        console.log(room?.player2);
+        console.log("New room created:", newRoomID, gameRooms.get(newRoomID));
+
+
+        }
+
+    }
+
+
+
 
     // Listen for "hello" event from client
     socket.on("hello", (msg) => {
         console.log("📨 Received from client:", msg);
         socket.emit("reply", "Hello from server!");
+
+
+        
+
+
     });
 
     // Listen for disconnect 
     socket.on("disconnect", () => {
         console.log("⚠️ Client disconnected:", socket.id);
+        playerID = playerID.filter(id => id !== socket.id);
+        console.log("All player IDs after disconnect:", playerID);
+
     });
 });
 
--- frontend/remoteGame.ts
+++ frontend/remoteGame.ts
@@ -2,18 +2,23 @@ import { io, Socket } from "socket.io-client";
 
 let socket: Socket | null = null;
 
+
+
 function connectServer() {
     socket = io("http://localhost:3001");
 
     socket.on("connect", () => {
         console.log("CONNECTED! socket Id ::", socket?.id);
         socket?.emit("hello", "Hi server!"); 
+       
     });
 
     socket.on("reply", (msg) => {
         console.log("Server replied:", msg);
     });
     
+    
+
     socket.on("disconnect", () => {
         console.log("DISCONNECTED! socket Id ::", socket?.id);
     });
